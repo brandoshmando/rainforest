900cc0f7e35607f0b5e2fdb79e7293a000b552a6
brandoshmando <brancraft@gmail.com>
2014-06-23 15:46:18 -0400

    Users model finished...I think. Password showing up as nil in the database but pretty sure that is ok??

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new()
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "You successfully created a new user!"
+    else
+      flash[:fail] = "New user was note created"
+      render :new
+    end
   end
 
   def end
