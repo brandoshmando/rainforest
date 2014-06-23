2b7e72c48839e8b9b410a3114cf88ea845824b40
brandoshmando <brancraft@gmail.com>
2014-06-23 17:01:00 -0400

    Removed default val from email column in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623190047) do
+ActiveRecord::Schema.define(version: 20140623205935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20140623190047) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",           default: "t"
+    t.string   "email"
   end
 
 end
