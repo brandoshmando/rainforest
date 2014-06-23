050ba5de37efa665348e4cbe5dd5a32d84dc6389
brandoshmando <brancraft@gmail.com>
2014-06-23 15:27:09 -0400

    Added email column to users table. Halfway through create method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new()
   end
 
   def end
   end
+
+  private
+
+    def user_params
+      params.require(:users).permit(:first_name, :last_name, :password, :email)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623181705) do
+ActiveRecord::Schema.define(version: 20140623190047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140623181705) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",           default: "t"
   end
 
 end
