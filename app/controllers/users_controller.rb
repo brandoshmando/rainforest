32328008ff5bd7d5e24e7eaaf358f245b599bc35
brandoshmando <brancraft@gmail.com>
2014-06-23 16:01:46 -0400

    Fixed user model with has_secure_password method to encrypt password and put into password digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:users).permit(:first_name, :last_name, :password, :email)
+      params.require(:user).permit(:first_name, :last_name, :password, :email)
     end
 end
