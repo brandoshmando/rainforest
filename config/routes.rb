70452afa827e7e304f16ee7841a513769bee7ad2
Brandon Craft <brancraft@gmail.com>
2014-06-23 21:59:52 -0400

    Added Product model and products controller along with the required actions as well as initial view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  resources :products
   resources :sessions, only: [:new, :create, :destroy]
-
   resources :users, only: [:new, :create, :show]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
