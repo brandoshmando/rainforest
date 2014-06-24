7dffac79380cc5c3b3d57b29e88ec3e915fb2e11
brandoshmando <brancraft@gmail.com>
2014-06-24 15:39:25 -0400

    Product model/controller done. Links setup to get around in proper UI form.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to product_path(@product) :notice => "Product Created!"
+      redirect_to product_path(@product), :notice => "Product Created!"
     else
       render :new
       flash.now[:alert] = "Product was not created for the following reasons"
@@ -22,12 +22,25 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+
+
+    if @product.update_attributes(product_params)
+      redirect_to @product
+    else
+      render :edit
+      flash.now[:alert] = "Your product was not updated..."
+    end
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to root_path
   end
 end
 
