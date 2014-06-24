1ca760e6d4be8969ab7eea039f80651d17a7351d
brandoshmando <brancraft@gmail.com>
2014-06-24 13:13:33 -0400

    Added logout link to navbar when logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+       session[:user_id] = user.id
       redirect_to products_path :notice => "Logged in!"
     else
       flash.now[:alert] = "Invalid email or pasword!"
