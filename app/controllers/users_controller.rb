fe17329c568a67647384dc05a5d8c80bc1e175e6
brandoshmando <brancraft@gmail.com>
2014-06-25 09:56:35 -0400

    Added delete path for user that only shows if user is logged in and is the user themself. Onto editing next

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    session[:user_id] = nil
+    @user.destroy
+    redirect_to users_path
+  end
+
   private
 
     def user_params
